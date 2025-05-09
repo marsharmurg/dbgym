cdc02233fd95937f2a67b5163acd05000c16f9f1
Marsharmur <mm220290@ues.edu.sv>
2025-05-08 21:58:42 -0600

    actualizacion

--- src/main/java/com/libcode/dbgym/data/Clase.java
+++ src/main/java/com/libcode/dbgym/data/Clase.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -24,6 +25,11 @@ public class Clase {
 
     @ManyToOne
     private Entrenador entrenador;
+
+    @ManyToOne
+    @JoinColumn(name = "entrenador_id")
+    private Entrenador entrenador;
+
 
 
     public Long getId() {
--- src/main/java/com/libcode/dbgym/data/Menbresia.java
+++ src/main/java/com/libcode/dbgym/data/Menbresia.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -26,7 +27,8 @@ public class Menbresia {
     private String descripcion;
 
     @ManyToOne
-    private Miembro miembro
+    @JoinColumn(name = "miembro_id")
+    private Miembro miembro;
 
     public Long getId() {
         return id;
